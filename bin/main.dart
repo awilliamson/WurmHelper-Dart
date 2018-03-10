9f9ed4c8cf2ea20f1adedc27e96bc59470cf8a1f
Ashley Williamson <11356993@students.lincoln.ac.uk>
2018-03-10 17:23:00 +0000

    Console-full from Stagehand

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:WurmHelper_Dart/WurmHelper_Dart.dart' as WurmHelper_Dart;
+
+main(List<String> arguments) {
+  print('Hello world: ${WurmHelper_Dart.calculate()}!');
+}
---
+++ test/WurmHelper_Dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:WurmHelper_Dart/WurmHelper_Dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
